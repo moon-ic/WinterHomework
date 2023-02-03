4c0f687fc8b008967678e2de767798193ddef2b4
moon-ic <3276817499@qq.com>
2023-02-04 01:03:34 +0800

    footer html&css

--- format/js/text.js
+++ format/js/text.js
@@ -22,27 +22,24 @@ let testRequest = new Request('http://why.vin:2023/toplis', {
 })
 //网络请求得到数据
 function getRankList() {
-    fetch("http://why.vin:2023/toplis")
+    fetch("http://why.vin:2023/comment/playlist?id=705123491")
         //这里是将数据json字符串转为对象
         .then(data => data.json())
         .then(json => {
-            //判断网络请求返回的数据是否正确
             if (json.code === 200) {
-                //如果正确就调用显示数据的方法
-                loadRankList(json.list)
+              
             } else {
                 console.log("请求错误" + json.code)
             }
         })
         .catch(reason => {
-            //请求失败的异常情况
             console.log("请求失败")
         })
 }
 
 //这里是初始化，你需要设置监听器，或者初始化一些网路数据
 function init() {
-    btn.onclick = getRankList
+     getRankList()
 }
 
 init()
--- play/play.js
+++ play/play.js
@@ -1,12 +1,11 @@
+const doc = document;
 //获取参数方法
 function GetUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
 }
-//使用获取参数方法
 const id = GetUrlParam("id");
-
 // 展示
 function getplayList(a) {
     fetch("http://why.vin:2023/song/detail?ids=" + a)
@@ -16,10 +15,10 @@ function getplayList(a) {
             //判断网络请求返回的数据是否正确
             if (json.code === 200) {
                 // 如果正确就调用显示数据的方法
-                document.querySelector(".cdImg>img").src = json.songs[0].al.picUrl;
-                document.querySelector(".name").innerHTML = json.songs[0].name;
-                document.querySelector(".singer").innerHTML = "歌手：" + json.songs[0].ar[0].name;
-                document.querySelector(".album").innerHTML = "专辑：" + json.songs[0].al.name;
+                doc.querySelector(".cdImg>img").src = json.songs[0].al.picUrl;
+                doc.querySelector(".name").innerHTML = json.songs[0].name;
+                doc.querySelector(".singer").innerHTML = "歌手：" + json.songs[0].ar[0].name;
+                doc.querySelector(".album").innerHTML = "专辑：" + json.songs[0].al.name;
 
             } else {
                 console.log("请求错误" + json.code)
@@ -46,7 +45,7 @@ function getLyrics(a) {
                 for (let i = 0; i < m.length; i++) {
                     lyc = lyc + m[i];
                 }
-                document.querySelector(".lyrics").innerHTML = lyc;
+                doc.querySelector(".lyrics").innerHTML = lyc;
             } else {
                 console.log("请求错误" + json.code)
             }
@@ -64,9 +63,9 @@ function playSong(a) {
             //判断网络请求返回的数据是否正确
             if (json.code === 200) {
                 // 如果正确就调用显示数据的方法
-                document.querySelector("#geImg>img").src = json.songs[0].al.picUrl;
-                document.querySelector("#geMing").innerHTML = json.songs[0].name;
-                document.querySelector("#geShou").innerHTML = json.songs[0].ar[0].name;
+                doc.querySelector("#geImg>img").src = json.songs[0].al.picUrl;
+                doc.querySelector("#geMing").innerHTML = json.songs[0].name;
+                doc.querySelector("#geShou").innerHTML = json.songs[0].ar[0].name;
             } else {
                 console.log("请求错误" + json.code)
             }
@@ -77,6 +76,32 @@ function playSong(a) {
         })
 }
 
+// cd动画
+// box.onclick=function(){
+//     // 获取当前盒子的动画播放状态
+//     let status = box.style.animationPlayState
+//     console.log(status)
+//     if(status==''|| status==='paused'){//暂停状态
+//         // 运行动画
+//         box.style.animationPlayState = 'running'
+//         // 播放音乐
+//         mv.play()
+//     }else{
+//         // 暂停音乐和动画
+//         box.style.animationPlayState = 'paused'
+//         mv.pause()
+//     }
+// }
+
+// DOM元素
+const audio = doc.querySelector('#audio'); // 播放器
+const controls = doc.querySelector('#controls'); // 按钮区域
+const title = doc.querySelector('#geMing'); // 歌曲标题
+const author = doc.querySelector('#geShou'); // 歌曲作者
+const playBtn = doc.querySelector('#play'); // 播放按钮
+const voiceBtn = doc.querySelector('#voice'); // 声音开关
+
+// 初始化
 function init() {
     getplayList(id);
     playSong(id);
