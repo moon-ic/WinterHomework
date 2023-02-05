ffe557d3a1b4ba164a30b9099e23f94116e8a0b1
moon-ic <3276817499@qq.com>
2023-02-05 22:26:01 +0800

    search

---
+++ playlistDetailComment/playlistDetailsC.js
@@ -0,0 +1,102 @@
+// 跳转
+function goToCom(a) {
+    window.location.href = "/playlistDetailComment/playDetailsComment.html?id=" + a;
+}
+function goToList(a) {
+    window.location.href = "/playlistDetails/playlistDetails.html?id=" + a;
+}
+
+//获取id参数方法
+function GetUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+const id = GetUrlParam("id");
+
+// 展示详情
+async function getplayList() {
+    let data = await fetch("http://why.vin:2023/playlist/detail?id=" + id)
+    let json = await data.json()
+    if (json.code === 200) {
+        //如果正确就调用显示数据的方法
+        if (json.fromUsers != null) document.querySelector(".from").innerText = json.fromUsers;
+        document.querySelector(".names").innerText = json.playlist.name;
+        document.querySelector(".introduceBox>img").src = json.playlist.coverImgUrl;
+        document.querySelector(".biaoqian").innerText = "标签： " + json.playlist.tags;
+        document.querySelector(".count").innerText = "播放： " + json.playlist.playCount;
+        document.querySelector(".songCount").innerText = "歌曲： " + json.playlist.trackIds.length;
+        document.querySelector(".des").innerText = json.playlist.description;
+    } else {
+        console.log("请求错误" + json.code)
+    }
+}
+// 展示评论
+let mainBox = document.querySelector(".display");
+function showCom() {
+    let section = document.createElement('div');
+    section.className = "com";
+    section.innerHTML =
+        ` 
+        <div class="Img">
+            <img src="/images/index1.png" alt="">
+        </div>
+        <div class="leftBox_">
+            <div class="comId">所拥有蓝水的陈桉:</div>
+            <div class="timeup">1:00</div>
+        </div>
+        <div class="commentAnswer">66</div>
+        <div class="answer">
+            <img src="/images/answer.png" alt="">
+        </div>
+   `
+    mainBox.appendChild(section);
+}
+async function getComment() {
+    let data = await fetch("http://why.vin:2023/comment/playlist?id=" + id)
+    let json = await data.json()
+    if (json.code === 200) {
+        document.querySelector(".comP").innerText = "最新评论(" + json.comments.length + ")";
+        for (let i = 0; i < json.comments.length; i++) {
+            showCom();
+            document.querySelectorAll(".comId")[i].innerText = json.comments[i].user.nickname + ": ";
+            document.querySelectorAll(".timeup")[i].innerText = json.comments[i].timeStr;
+            document.querySelectorAll(".commentAnswer")[i].innerText = json.comments[i].content;
+            document.querySelectorAll(".Img>img")[i].src = json.comments[i].user.avatarUrl;
+        }
+    } else {
+        console.log("请求错误" + json.code)
+    }
+}
+//进行评论 
+let con = document.querySelector(".write").innerText;
+async function commentIt() {
+    let data = await fetch("/comment?t=1&type=2&id=" + id + "&content=" + con)
+    let json = await data.json()
+    if (json.code === 200) {
+        showCom();
+        // document.querySelectorAll(".comId")[i].innerText = json.comments[i].user.nickname + ": ";
+        document.querySelectorAll(".commentAnswer")[i].innerText = con;
+        // document.querySelectorAll(".Img>img")[i].src = json.comments[i].user.avatarUrl;
+    } else {
+        console.log("请求错误" + json.code)
+    }
+}
+//初始化
+async function init() {
+    // 展示
+    let res = await getplayList();
+    let res1 = await getComment()
+    // 跳转
+    document.querySelector("#geIndex").onclick = () => {
+        goToList(id);
+    }
+    document.querySelector("#comment").onclick = () => {
+        goToCom(id);
+    }
+    document.querySelector("#comBtn").onclick = () => {
+
+    }
+}
+
+init();
---
+++ search/search.js
@@ -0,0 +1,49 @@
+function goTo(a) {
+    window.location.href = "/play/play.html?id=" + a;
+}
+
+function showSear() {
+    let mainBox = document.querySelector(".searchMain");
+    let section = document.createElement('div');
+    section.className = "searchSong";
+    section.innerHTML =
+        ` <div class="searchNums"></div>
+        <div class="searTitle">音乐标题</div>
+        <div class="searSinger">歌手</div>
+        <div class="searAlbum">专辑</div>`
+    mainBox.appendChild(section);
+}
+
+let id = [];
+let m;
+async function getSearch() {
+    let data = await fetch("http://why.vin:2023/search?keywords=海阔天空")
+    let json = await data.json()
+    if (json.code === 200) {
+        for (let i = 0; i < json.result.songs.length; i++) {
+            doc.querySelectorAll(".searTitle")[0].style.color = "#989898";
+            showSear();
+            doc.querySelectorAll(".searchNums")[i + 1].innerText = i + 1;
+            doc.querySelectorAll(".searTitle")[i + 1].innerText = json.result.songs[i].name;
+            doc.querySelectorAll(".searSinger")[i + 1].innerText = json.result.songs[i].artists[0].name;
+            doc.querySelectorAll(".searAlbum")[i + 1].innerText = json.result.songs[i].album.name;
+            id[i] = json.result.songs[i].id;
+            m = json.result.songs.length;
+        }
+    } else {
+        console.log("请求错误" + json.code);
+    }
+    return { id, m }
+}
+
+// 初始化
+async function init() {
+    let res = await getSearch();
+    // 跳转
+    // 歌曲
+    for (let i = 0; i < m; i++) {
+        document.querySelectorAll(".searchSong")[i].onclick = () => goTo(id[i]);
+    }
+}
+
+init();
